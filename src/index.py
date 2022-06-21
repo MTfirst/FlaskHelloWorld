f801d936f99e1260374f0c8042920d643286295f
mtfirst <34882432+MTfirst@users.noreply.github.com>
2022-06-22 01:02:04 +0900

    add formatter

--- src/index.py
+++ src/index.py
@@ -1,29 +1,35 @@
-from flask import Flask, render_template, jsonify
-import subprocess
 import json
+import subprocess
+
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 
 def get_pip_freeze():
-    res = subprocess.run('pip freeze', shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    res = subprocess.run(
+        "pip freeze", shell=True, stdout=subprocess.PIPE, encoding="utf-8"
+    )
     pip_list = res.stdout.splitlines()
     return pip_list
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/get_pip')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/get_pip")
 def get_pip():
     pip_list = get_pip_freeze()
-    return render_template('get_pip.html', pip_list = pip_list)
+    return render_template("get_pip.html", pip_list=pip_list)
 
-@app.route('/get_pip_ajax', methods=["POST"])
+
+@app.route("/get_pip_ajax", methods=["POST"])
 def get_pip_ajax():
-    print('get_pip_ajax')
     pip_list = get_pip_freeze()
     return jsonify(pip_list)
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
